6914a713636539ca544fcc1a9714539f82afc5d2
Davizao <a14794@oficina.pt>
2026-05-20 11:01:26 +0000

    final

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Professor from './Professor'
 import App from './App.jsx'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 //import Message from './Message.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header />
     <App />
-    <Professor />
+    <Footer />
   </StrictMode>,
 )
