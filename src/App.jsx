a1cc0dbc84106c7412fd732d64f51b846ab8da32
Davizao <a14794@oficina.pt>
2026-05-22 11:51:36 +0000

    oioi

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Mensagem from './Mensagem';
 
 function App() {
   return (
@@ -14,10 +15,14 @@ function App() {
       {/* Evento onClick em React usa camelCase e chama uma função */}
       <button
         className="btn btn-success d-block mx-auto"
-        onClick={() => alert('Olá, mundo!')}
+        onClick={() => alert('Olá, Davi!')}
       >
         Clica-me!
       </button>
+      <h1 className="text-center">Componentes e Props</h1>
+      {/* Cada componente recebe props diferentes */}
+      <Mensagem myText="Bem-vindo à tua primeira aplicação React!" />
+      <Mensagem myText="Os componentes permitem reutilizar código!" />
     </div>
   );
 }
