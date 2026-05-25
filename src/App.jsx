1ed211621f648ceb120b87deb60bbc17884d4d46
Davizao <a14794@oficina.pt>
2026-05-25 09:39:58 +0000

    final

--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,9 @@ import Alerta from './Alerta';
 
 function App() {
   return (
-    <div className="container mt-4">
-      <h1 className="text-center text-primary">Olá React!</h1>
-      <p className="lead text-center">
-        Este é o teu primeiro componente React.
-      </p>
-      {/* Evento onClick em React usa camelCase e chama uma função */}
-      <button
-        className="btn btn-success d-block mx-auto"
-        onClick={() => alert('Olá, Davi!')}
-      >
-        Clica-me!
-      </button>
-
-      <Contador />
-      <h1 className="text-center">Componentes e Props</h1>
-      {/* Cada componente recebe props diferentes */}
-      <Mensagem myText="Bem-vindo à tua primeira aplicação React!" />
-      <Mensagem myText="Os componentes permitem reutilizar código!" />
-
-
-      <Alerta tipo="success" texto="O Dinis" />
-      <Alerta tipo="danger" texto="é muito" />
-      <Alerta tipo="warning" texto="muito buro bradier" />
-
-      <Formulario/>
-      <Notas/>
-
-    </div>
+    
+      <h1 className="text-center">Notas</h1>
+      
   );
 }
 
